8b31dffbbeec3c5c7173c0f416e29b3b7c1b82e2
Arseniy Kapshtyk <kapshtykas@yandex.ru>
2023-06-15 00:21:04 +0300

    update docker-compose

--- backend/migrate_run_app.sh
+++ backend/migrate_run_app.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
 
-while ! nc -z db 5432; do
-    sleep 0.5
-    echo "Waiting for the database..."
-done
-echo "Connected to the database"
-
-
-python manage.py migrate
-python manage.py loaddata database.json
-python manage.py collectstatic --noinput
-gunicorn -w 2 -b 0:8000 foodgram.wsgi
+gunicorn -w 2 -b 0:8000 backend.foodgram.wsgi
